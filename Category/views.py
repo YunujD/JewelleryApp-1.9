4a24094133a5d2a2e556f278b93dccc04e883fa1
rohanneps <rohanneps@gmail.com>
2016-01-20 13:51:17 +0545

    unrequired removed

--- Category/views.py
+++ Category/views.py
@@ -3,14 +3,5 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from Product.mixin import LoginRequiredMixin
 from .models import Category
-from Product.templates import Product
 
 # Create your views here.
-
-
-class CategoryDetailView(LoginRequiredMixin,DetailView):
-	model=Category
-
-class CategoryListView(LoginRequiredMixin,ListView):
-	model=Category
-	queryset=Category.objects.all()
